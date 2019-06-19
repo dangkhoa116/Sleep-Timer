ee72eedacac5478aced5cc4be1d68082853d327c
dangkhoa116 <dangkhoa116@gmail.com>
2019-06-19 17:03:03 +0700

    add turn off audio feature

--- app/src/main/java/com/example/practiceui2/MainActivity.java
+++ app/src/main/java/com/example/practiceui2/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.practiceui2;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.mikhaellopez.circularprogressbar.CircularProgressBar;
 
@@ -23,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     private Button minus10;
     private Button startBnt;
     private Button endBnt;
+    private Button extendBnt;
+    private CountDownTimer myCountDownTimer;
+    private AudioManager am;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         minus10 = findViewById(R.id.minus10);
         startBnt = findViewById(R.id.startBnt);
         endBnt = findViewById(R.id.endBnt);
+        extendBnt = findViewById(R.id.extendBnt);
 
     }
 
@@ -55,37 +59,84 @@ public class MainActivity extends AppCompatActivity {
     }
     public void disappearedButton()
     {
-        minusMinutesBnt.setVisibility(View.GONE);
-        plusMinutesBnt.setVisibility(View.GONE);
-        plus1.setVisibility(View.GONE);
-        plus10.setVisibility(View.GONE);
-        minus1.setVisibility(View.GONE);
-        minus10.setVisibility(View.GONE);
-        circularProgressBar.setVisibility(View.VISIBLE);
+            minusMinutesBnt.setVisibility(View.INVISIBLE);
+            plusMinutesBnt.setVisibility(View.INVISIBLE);
+            plus1.setVisibility(View.INVISIBLE);
+            plus10.setVisibility(View.INVISIBLE);
+            minus1.setVisibility(View.INVISIBLE);
+            minus10.setVisibility(View.INVISIBLE);
+            endBnt.setVisibility(View.VISIBLE);
+            extendBnt.setVisibility(View.VISIBLE);
+            circularProgressBar.setVisibility(View.VISIBLE);
+    }
+    public void appearButton()
+    {
+        minusMinutesBnt.setVisibility(View.VISIBLE);
+        plusMinutesBnt.setVisibility(View.VISIBLE);
+        plus1.setVisibility(View.VISIBLE);
+        plus10.setVisibility(View.VISIBLE);
+        minus1.setVisibility(View.VISIBLE);
+        minus10.setVisibility(View.VISIBLE);
+        endBnt.setVisibility(View.INVISIBLE);
+        extendBnt.setVisibility(View.INVISIBLE);
+        startBnt.setVisibility(View.VISIBLE);
+        circularProgressBar.setVisibility(View.INVISIBLE);
     }
     public void startCountDown(View view)
     {
        disappearedButton();
        startBnt.setVisibility(View.INVISIBLE);
        endBnt.setVisibility(View.VISIBLE);
-        minLeft = Integer.parseInt((String) minutesLeft.getText());
-        circularProgressBar.setProgressMax(minLeft);
+       extendBnt.setVisibility(View.VISIBLE);
+       minLeft = Integer.parseInt((String) minutesLeft.getText());
+       circularProgressBar.setProgressMax(minLeft*60);
 
-        new CountDownTimer(minLeft*60000, 1000*60) {
+       myCountDownTimer = new CountDownTimer(minLeft*60000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-                circularProgressBar.setProgress(millisUntilFinished/60000);
+                circularProgressBar.setProgress(millisUntilFinished/1000);
                 minutesLeft.setText(Long.toString(millisUntilFinished/60000));
             }
-
             @Override
             public void onFinish() {
-                minutesLeft.setText("0");
+                myCountDownTimer.cancel();
+                appearButton();
+                minutesLeft.setText("1");
+                turnOffAudio();
             }
-        }.start();
+       }.start();
+    }
+    public void endButton(View view)
+    {
+        myCountDownTimer.cancel();
+        appearButton();
+        minutesLeft.setText("1");
 
     }
-
-
-
+    public void extendButton(View view)
+    {
+        myCountDownTimer.cancel();
+        minLeft = Integer.parseInt((String) minutesLeft.getText());
+        minLeft+=5;
+        circularProgressBar.setProgressMax(minLeft*60);
+        myCountDownTimer = new CountDownTimer(minLeft*60000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                circularProgressBar.setProgress(millisUntilFinished/1000);
+                minutesLeft.setText(Long.toString(millisUntilFinished/60000));
+            }
+            @Override
+            public void onFinish() {
+                myCountDownTimer.cancel();
+                appearButton();
+                minutesLeft.setText("1");
+                turnOffAudio();
+            }
+        }.start();
+    }
+    public void turnOffAudio()
+    {
+        am = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        am.setStreamMute(AudioManager.STREAM_MUSIC, true);
+    }
 }
